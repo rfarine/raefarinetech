afd5d23b47cfbae87d0128adb0505fe0334089ca
Rae Farine <rfarine1027@gmail.com>
2016-11-20 19:19:27 -0500

    normalize and add fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import Layout from './layout/Layout';
+import Layout from './components/Layout/Layout';
+import './assets/styles/global.scss';
 
 export default function App() {
   return (
     <Layout>
-      <div>Hello World!</div>
+      <div>
+        <h1>Hello World!</h1>
+        <p>
+          Flexitarian narwhal cold-pressed, post-ironic YOLO succulents
+          try-hard viral marfa sriracha street art locavore intelligentsia.
+          Banh mi etsy craft beer, yuccie put a bird on it gochujang bespoke yr.
+          Craft beer helvetica blue bottle, man braid banjo 90's single-origin
+          coffee cold-pressed trust fund. Food truck fap tattooed tote bag,
+          wayfarers woke kitsch yr. 3 wolf moon try-hard asymmetrical,
+          raclette intelligentsia bitters pop-up tilde stumptown butcher
+          microdosing retro humblebrag. Pabst VHS keffiyeh, street art
+          tattooed PBR&B green juice poke. Hoodie wolf iPhone hell of,
+          kombucha cred shabby chic vegan.
+        </p>
+      </div>
     </Layout>
   );
 };
